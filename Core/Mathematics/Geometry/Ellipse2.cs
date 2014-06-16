0de0991079141e531fca857d84c910644b80f051
Douglas McClean <douglas.mcclean@gmail.com>
2014-06-16 16:51:25 -0400

    Added basic ellipse type.

---
+++ Core/Mathematics/Geometry/Ellipse2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutomationLibrary.Mathematics.Geometry
+{
+    [Serializable]
+    public sealed class Ellipse2
+    {
+        private readonly Vector2 _center;
+        private readonly double _semiMajorAxis;
+        private readonly double _semiMinorAxis;
+        private readonly double _angleOfSemiMajorAxis;
+
+        public Ellipse2(Vector2 center, double semiMajorAxis, double semiMinorAxis, double angle)
+        {
+            _center = center;
+            _semiMajorAxis = semiMajorAxis;
+            _semiMinorAxis = semiMinorAxis;
+            _angleOfSemiMajorAxis = angle;
+        }
+    }
+}
